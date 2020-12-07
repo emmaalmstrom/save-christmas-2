5f80c76fb19a53f4d39644288c87ea5966538d69
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-12-07 16:04:40 +0100

    07-12-2020

---
+++ playground.js
@@ -0,0 +1,30 @@
+import { randomArray } from './helpers.js'
+
+const a = randomArray(100, 9)
+
+console.log(a.sort())
+
+const storage = [
+    'Bicycle',
+    'Lego',
+    'Doll',
+    'Teddybear',
+    'Crayons',
+    'Fishing rod'
+]
+
+console.log(storage.sort())
+
+
+const kids = [
+    { name: 'Elias', age: 11, naughtyScore: 2, wish: 'Bicycle' },
+    { name: 'Lotta', age: 9, naughtyScore: 7, wish: 'Hamster' },
+    { name: 'Kim', age: 9, naughtyScore: 2, wish: 'Lego' },
+    { name: 'José', age: 12, naughtyScore: 9, wish: 'Snowboard' },
+    { name: 'Alexandra', age: 12, naughtyScore: 6, wish: 'Survival kit' },
+    { name: 'Lisa', age: 10, naughtyScore: 4, wish: 'Lego' }
+]
+
+console.log(kids.sort(function(a, b) {
+    return parseFloat(a.naughtyScore) - parseFloat(b.naughtyScore);
+}))
--- sorting.js
+++ sorting.js
@@ -4,22 +4,20 @@ const a = randomArray(100, 10)
 
 import { swap } from './helpers.js'
 
-/*
+
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[i] < arr[i + 1]) {
-                swap(arr, arr[i], arr[i + 1])
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1)
         }
         }
     }
-    return arr
 }
 
+bubbleSort(a)
 console.log(a)
 
-console.log(bubbleSort(a))
-*/
 
 
 for (let j = 0; j < a.length - 1; j++) {
